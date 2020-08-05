9f6443c3ea331a3b7b63f65640a388e6175c145f
KatiePDX <61799774+katiepdx@users.noreply.github.com>
2020-08-05 14:20:49 -0700

    add SearchBar component to App.js and styles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import HeaderBar from './HeaderBar';
+import SearchBar from './SearchBar';
 
 export default class App extends React.Component {
   render() {
     return (
       <div>
         <HeaderBar/>
+        <SearchBar/>
       </div>
     )
   }
